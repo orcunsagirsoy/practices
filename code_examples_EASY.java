e3a24bae49d785c7392c78b614d9d000aab497b7
Orcun <orcun@mindbehind.com>
2022-11-13 01:02:08 +0300

    word pattern

--- code_examples_EASY.java
+++ code_examples_EASY.java
@@ -7,6 +7,37 @@
 // Merge two binary trees - Solve with JAVA
 //Arranging coins - JAVA
 
+
+// WORD PATTERN
+function wordPattern(pattern: string, s: string): boolean {
+  const patternMap = new Map<string, string>();
+  const wordsMap = new Map<string, string>();
+  const patternLen = pattern.length;
+  const words = s.split(" ");
+  if (patternLen != words.length) {
+    return false;
+  }
+  for (let i = 0; i < patternLen; i++) {
+    const ch = pattern[i];
+    const word = words[i];
+    if (patternMap.has(ch)) {
+      if (patternMap.get(ch) != word) {
+        return false;
+      }
+    } else {
+      patternMap.set(ch, word);
+    }
+    if (wordsMap.has(word)) {
+      if (wordsMap.get(word) != ch) {
+        return false;
+      }
+    } else {
+      wordsMap.set(word, ch);
+    }
+  }
+  return true;
+};
+
 //INTERSECTION OF TWO ARRAYS
 function intersection(nums1: number[], nums2: number[]): number[] {
   const set = new Set();
